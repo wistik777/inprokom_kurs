0b794cafa41918c7f0cc784a9f378d17ad2fdf6d
wistik777 <lgame0043@gmail.com>
2026-03-06 14:32:14 +0300

    С менеджерами

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -44,11 +44,26 @@ function Header({ name }) {
             <div className={`w-full transition-all duration-300 ${hideMenuRow ? 'h-[96px]' : 'h-[180px]'}`} />
 
             <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-[0_2px_10px_rgba(0,0,0,0.08)]">
-                <div className="flex justify-around mt-4">
+                <div className="mt-4 flex items-center justify-around">
                     <button className="cursor-pointer"><img src="/img/logo.svg" alt="Инпроком" /></button>
-                    <div className="flex gap-29 items-center">
-                        <p>info@inprokom.ru</p>
-                        <p>8 49244 7 75 34</p>
+                    <div className="flex items-center gap-8">
+                        <div className="relative min-h-[32px] min-w-[820px]">
+                            <div className={`absolute inset-0 flex items-center gap-10 transition-all duration-300 ${hideMenuRow ? 'pointer-events-none -translate-y-2 opacity-0' : 'translate-y-0 opacity-100'}`}>
+                                <p>info@inprokom.ru</p>
+                                <p>8 49244 7 75 34</p>
+                            </div>
+                            <div className={`absolute inset-0 flex items-center gap-6 text-[14px] decoration-0
+                            [&_a]:whitespace-nowrap [&_a]:transition-all [&_a]:duration-300 [&_a]:ease-in-out
+                            [&_a:hover]:text-[#FA4234] transition-all duration-300 ${hideMenuRow ? 'translate-y-0 opacity-100' : 'pointer-events-none translate-y-2 opacity-0'}`}>
+                                <a href="/">ГЛАВНАЯ</a>
+                                <a href="" onMouseEnter={()=> setShowCompany(true)} onMouseLeave={()=> setShowCompany(false)}>О КОМПАНИИ</a>
+                                <a href="/catalog">КАТАЛОГ</a>
+                                <a href="">ПРЕСС-ЦЕНТР</a>
+                                <a href="">КОНТАКТЫ</a>
+                                <a href="">ВАКАНСИИ</a>
+                                <a href="">НЕЛИКВИДЫ И ОСТАТКИ СКЛАДОВ</a>
+                            </div>
+                        </div>
                         <a href="/cart" className="relative cursor-pointer">
                             <img src="/img/basket.svg" alt="Корзина" className="h-[50px]" />
                             {cartCount > 0 && (
